d3769573ef8aace920be646fe0b59ec55434e2a0
Igor9225841 <igor922584@yandex.ru>
2021-08-08 19:50:45 +0300

    Компилируется без ошибок и замечаний, но расчет числа байт не выполняется

--- lesson10/main.c
+++ lesson10/main.c
@@ -5,86 +5,88 @@
 
 struct data
 	{
-		char *ch[1000];
+		char ch[100];
 	};
 
-
-int readf (char fname1, struct data *p);
-int writef (char fname2, struct data *p);
-void print_er (int er);
-
+int readf (struct data *u);
+int writef (struct data *u);
+void print_rez (int rez1, int rez2);
 
 int main (int argc, char **argv)
 {
-	
 	struct data data_file; 
-	p_data = &data_file;
-	int rez=0;
-	char fname1[100];
-	char fname2[100];
-	
-	printf("Enter source and destination files\n");
-	scanf("%c%c", fname1, fname2);
-	readf (fname1, &data_file);
-	rez= writef (fname2, &data_file);
-	printf("Copy done. Transfered %d\n", rez);	
+	int rez1=0;
+	int rez2=0;
+//	char fname1[10];
+//	char fname2[10];
+//	printf("Enter source and destination files\n");
+//	scanf("%c%c", fname1, fname2);
+	rez1= readf (&data_file);
+	rez2= writef (&data_file);
+	print_rez(rez1, rez2);
+
     return 0;
 }
 
-int readf (char fname1, struct data *p)
-{
-	FILE *f;
-//	char ch;
+int readf (struct data *u)
+{	
+	signed char c;
 	int i=0;
-	
-	f= fopen(fname1, "r");
-	if (f!=NULL)
+	FILE *f;
+	f= fopen("file1", "r");
+	if (f==NULL)
 	{
 		i=-1;
-		print_er(i);
-		return;
+		return i;
 	}
 	
-	while (fscanf(f, "%c", *p[i]->ch) !=EOF)
+	while ((c=fgetc(f)) !=EOF)
 	{
+		u->ch[i]= c;
 		i++;
 	}
 	
-	fclose(f);
-	return;	
-}
-
-int writef (char fname2, struct data *p)
-{
-	FILE *f;
-	int i=0;
-	
-	f= fopen(fname2, "w");
-	if (f!=NULL)
-	{
-		i=-2;
-		print_er(i);
-		return;
-	}
-	
-	while (fprintf(f, "%c", *p[i]->ch) !=EOF)
-	{
-		i++;
-	}
-	i= ftell(f);
 	fclose(f);
 	return i;	
 }
 
-void print_er(int er)
+int writef (struct data *u)
 {
-	if (er == -1)
+	int i=0;
+	signed char c;
+	FILE *f;
+	f= fopen("file2", "w");
+	if (f==NULL)
+	{
+		i=-2;
+		return i;
+	}
+	
+	while ((c=fgetc(f)) !=EOF) 
+	{
+		u->ch[i]= c;
+		i++;
+	}
+	fclose(f);
+	return i;	
+}
+
+void print_rez(int rez1, int rez2)
+{
+	if (rez1 == -1)
 	{
 		printf("Error. File1 not exists\n");
 	}
-	else if (er == -2)
+	if (rez2 == -2)
 	{
 		printf("Error. File2 not exists\n");
 	}
+	if (rez2>= 0)
+	{
+		printf("Copy done. Transfered %d\n", rez2);
+	}
 	return;
 };
+
+
+
