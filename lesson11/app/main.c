7fc8d990b38cb9dd2b3ebae9d737a22ca1165e34
Igor9225841 <88245782+Igor9225841@users.noreply.github.com>
2021-08-15 17:52:32 +0300

    Update main.c
    Удалил закомментированные строки

--- lesson11/app/main.c
+++ lesson11/app/main.c
@@ -3,23 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <sys/stat.h>
 
 #include <list_of_files.h>
 
 int main (int argc, char **argv)
 {
-/*	struct files_name list_files; 
-	int rez1=0;
-*/
 	char path_dir[255]={0};
 	printf("Enter the full path to the directiry\n");
 	scanf("%s", path_dir);
-	
-//	getcwd (path_dir, 255);
-    ls_files (path_dir);
+        ls_files (path_dir);
     
-    return 0;
+        return 0;
 }
 
 
